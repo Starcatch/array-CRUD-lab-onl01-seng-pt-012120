9fe4bed513df96283593dea7d78a17348c0c410b
Timur Mishiev <timurmishiev@gmail.com>
2020-01-27 03:07:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,15 @@
-def create_an_empty_array = []
+def create_an_empty_array 
+  []
   
 end
 
-def create_an_array = ["First", "Second", "Third", "Fourth"]
+def create_an_array 
+  ["First", "Second", "Third", "Fourth"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("element")
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
